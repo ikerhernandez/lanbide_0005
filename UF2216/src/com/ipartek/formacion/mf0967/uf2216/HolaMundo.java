afb9c912469b4c91652a41b8a2fdb2a051ea91e9
java <java@GORBEIA13.ipartekaula.com>
2022-09-20 08:54:19 +0200

    Lenguaje

--- UF2216/src/com/ipartek/formacion/mf0967/uf2216/HolaMundo.java
+++ UF2216/src/com/ipartek/formacion/mf0967/uf2216/HolaMundo.java
@@ -3,7 +3,6 @@ package com.ipartek.formacion.mf0967.uf2216;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Hola Mundo");
 	}
---
+++ UF2216/src/com/ipartek/formacion/mf0967/uf2216/Lenguaje.java
@@ -0,0 +1,15 @@
+package com.ipartek.formacion.mf0967.uf2216;
+
+public class Lenguaje {
+
+	public static void main(String[] args) {
+
+		double d1 = 4.8;
+		double d2 = 0.4;
+		
+		long l = 1234123123123L;
+		
+		System.out.println(d1 - d2);
+	}
+
+}
